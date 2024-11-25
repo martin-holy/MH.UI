b89b96d274534a122cb864561c88a402d9524683
martin-holy <martin.holy@hotmail.com>
2024-11-25 13:45:28 +0100

    [C] SlidePanel: code style

--- src/MH.UI/Controls/SlidePanel.cs
+++ src/MH.UI/Controls/SlidePanel.cs
@@ -13,9 +13,9 @@ public class SlidePanel : ObservableObject {
 
   public object Content { get; }
   public Dock Dock { get; }
-  public bool CanOpen { get => _canOpen; set { _canOpen = value; OnCanOpenChanged(); } }
-  public bool IsOpen { get => _isOpen; set => OnIsOpenChanged(value); }
-  public bool IsPinned { get => _isPinned; set { _isPinned = value; OnIsPinnedChanged(); } }
+  public bool CanOpen { get => _canOpen; set { _canOpen = value; _onCanOpenChanged(); } }
+  public bool IsOpen { get => _isOpen; set => _onIsOpenChanged(value); }
+  public bool IsPinned { get => _isPinned; set { _isPinned = value; _onIsPinnedChanged(); } }
   public double Size { get => _size; private set { _size = value; OnPropertyChanged(); } }
 
   public SlidePanel(Dock dock, object content, double size) {
@@ -24,28 +24,28 @@ public class SlidePanel : ObservableObject {
     Size = size;
   }
 
-  private void OnCanOpenChanged() =>
-    IsOpen = CanOpen && IsPinned;
+  private void _onCanOpenChanged() =>
+    IsOpen = _canOpen && _isPinned;
 
-  private void OnIsOpenChanged(bool value) {
+  private void _onIsOpenChanged(bool value) {
     if (value.Equals(_isOpen)) return;
     _isOpen = value;
-    if (!IsOpen && IsPinned) IsPinned = false;
+    if (!_isOpen && _isPinned) IsPinned = false;
     OnPropertyChanged(nameof(IsOpen));
   }
 
-  private void OnIsPinnedChanged() {
+  private void _onIsPinnedChanged() {
     OnPropertyChanged(nameof(IsPinned));
-    IsOpen = IsPinned;
+    IsOpen = _isPinned;
   }
 
   public void SetSize(double size) {
-    if (size != 0 && !size.Equals(Size)) Size = size;
+    if (size != 0 && !size.Equals(_size)) Size = size;
   }
 
   public void OnMouseMove(Func<double, bool> mouseOut, bool mouseOnEdge) {
-    if (IsPinned) return;
-    if (mouseOut(Size)) IsOpen = false;
-    else if (mouseOnEdge && CanOpen) IsOpen = true;
+    if (_isPinned) return;
+    if (mouseOut(_size)) IsOpen = false;
+    else if (mouseOnEdge && _canOpen) IsOpen = true;
   }
 }
