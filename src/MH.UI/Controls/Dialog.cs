3d954dde4b8717b61fc53e78f71bb0ef7c77a9a1
martin-holy <martin.holy@hotmail.com>
2025-04-11 18:08:28 +0200

    [U] Dialog: set Show and ShowAsync implementations methods

--- src/MH.UI/Controls/Dialog.cs
+++ src/MH.UI/Controls/Dialog.cs
@@ -10,13 +10,13 @@ public class Dialog(string title, string icon) : ObservableObject {
   private string _icon = icon;
   private int _result = -1;
   private DialogButton[] _buttons = [];
+  private static Func<Dialog, int>? _show;
+  private static Func<Dialog, Task<int>>? _showAsync;
 
   public string Title { get => _title; set { _title = value; OnPropertyChanged(); } }
   public string Icon { get => _icon; set { _icon = value; OnPropertyChanged(); } }
   public DialogButton[] Buttons { get => _buttons; set { _buttons = value; OnPropertyChanged(); } }
-  public TaskCompletionSource<int> TaskCompletionSource { get; } = new();
-  public static Func<Dialog, int> Show { get; set; } = _ => -1;
-  public static Func<Dialog, Task<int>> ShowAsync { get; set; } = _ => Task.FromResult(-1);
+  public TaskCompletionSource<int> TaskCompletionSource { get; private set; } = new();
 
   public int Result {
     get => _result;
@@ -44,4 +44,22 @@ public class Dialog(string title, string icon) : ObservableObject {
     new(() => Result = result, icon, text);
 
   protected virtual Task _onResultChanged(int result) => Task.CompletedTask;
+
+  public static int Show(Dialog dialog) {
+    if (_show == null) throw new NotImplementedException(nameof(_show));
+    dialog.TaskCompletionSource = new();
+    return _show(dialog);
+  }
+
+  public static void SetShowImplementation(Func<Dialog, int> func) =>
+    _show = func;
+
+  public static Task<int> ShowAsync(Dialog dialog) {
+    if (_showAsync == null) throw new NotImplementedException(nameof(_showAsync));
+    dialog.TaskCompletionSource = new();
+    return _showAsync(dialog);
+  }
+
+  public static void SetShowAsyncImplementation(Func<Dialog, Task<int>> func) =>
+    _showAsync = func;
 }
