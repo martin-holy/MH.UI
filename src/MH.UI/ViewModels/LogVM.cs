1c432b128d6283b4de6b5a036f71efaae38e4c67
martin-holy <martin.holy@hotmail.com>
2025-09-20 10:02:17 +0200

    [N] LogVM

---
+++ src/MH.UI/ViewModels/LogVM.cs
@@ -0,0 +1,12 @@
+﻿using MH.Utils.BaseClasses;
+using System.Collections.ObjectModel;
+
+namespace MH.UI.ViewModels;
+public sealed class LogVM : ObservableObject {
+  public ObservableCollection<LogItem> Items => MH.Utils.Log.Items;
+  public RelayCommand ClearCommand { get; }
+
+  public LogVM() {
+    ClearCommand = new RelayCommand(() => Items.Clear(), null, "Clear");
+  }
+}
