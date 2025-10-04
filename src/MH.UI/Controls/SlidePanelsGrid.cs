dfc98760dac7ac777d51d70d092c94337b17459d
martin-holy <martin.holy@hotmail.com>
2025-10-04 08:45:26 +0200

    [C] SlidePanelsGrid

--- src/MH.UI/Controls/SlidePanelsGrid.cs
+++ src/MH.UI/Controls/SlidePanelsGrid.cs
@@ -15,11 +15,11 @@ public class SlidePanelsGrid : ObservableObject {
 
   public ISlidePanelsGridHost? Host { get => _host; set => _setHost(value); }
   public int ActiveLayout { get => _activeLayout; set => _onActivateLayoutChanged(value); }
-  public bool[][] PinLayouts { get; }
-  public SlidePanel? PanelLeft { get; }
-  public SlidePanel? PanelTop { get; }
-  public SlidePanel? PanelRight { get; }
-  public SlidePanel? PanelBottom { get; }
+  public bool[][] PinLayouts { get; set; }
+  public SlidePanel PanelLeft { get; }
+  public SlidePanel PanelTop { get; }
+  public SlidePanel PanelRight { get; }
+  public SlidePanel PanelBottom { get; }
   public object PanelMiddle { get; }
 
   public static RelayCommand<SlidePanel> PinCommand { get; } = new(x => x!.IsPinned = !x.IsPinned, x => x != null);
@@ -50,10 +50,10 @@ public class SlidePanelsGrid : ObservableObject {
     _activeLayout = value;
     OnPropertyChanged(nameof(ActiveLayout));
     var activeLayout = PinLayouts[value];
-    if (PanelLeft != null) PanelLeft.IsPinned = activeLayout[0];
-    if (PanelTop != null) PanelTop.IsPinned = activeLayout[1];
-    if (PanelRight != null) PanelRight.IsPinned = activeLayout[2];
-    if (PanelBottom != null) PanelBottom.IsPinned = activeLayout[3];
+    PanelLeft.IsPinned = activeLayout[0];
+    PanelTop.IsPinned = activeLayout[1];
+    PanelRight.IsPinned = activeLayout[2];
+    PanelBottom.IsPinned = activeLayout[3];
   }
 
   private void _setHost(ISlidePanelsGridHost? host) {
@@ -72,9 +72,9 @@ public class SlidePanelsGrid : ObservableObject {
     // to stop opening/closing panel by itself in some cases
     if (e.Position is { X: 0, Y: 0 } || e.Position.X < 0 || e.Position.Y < 0) return;
 
-    PanelLeft?.OnGridMouseMove(size => e.Position.X > size, e.Position.X < 5);
-    PanelTop?.OnGridMouseMove(size => e.Position.Y > size, e.Position.Y < 5);
-    PanelRight?.OnGridMouseMove(size => e.Position.X < e.Width - size, e.Position.X > e.Width - 5);
-    PanelBottom?.OnGridMouseMove(size => e.Position.Y < e.Height - size, e.Position.Y > e.Height - 5);
+    PanelLeft.OnGridMouseMove(size => e.Position.X > size, e.Position.X < 5);
+    PanelTop.OnGridMouseMove(size => e.Position.Y > size, e.Position.Y < 5);
+    PanelRight.OnGridMouseMove(size => e.Position.X < e.Width - size, e.Position.X > e.Width - 5);
+    PanelBottom.OnGridMouseMove(size => e.Position.Y < e.Height - size, e.Position.Y > e.Height - 5);
   }
 }
