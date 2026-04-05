20854ca70c212399f444f63c97a1d5fad4d77a97
martin-holy <martin.holy@hotmail.com>
2026-04-05 14:28:25 +0200

    [N] Transform2D struct

---
+++ src/MH.UI/Primitives/Transform2D.cs
@@ -0,0 +1,15 @@
+﻿namespace MH.UI.Primitives;
+
+public struct Transform2D {
+  public float ScaleX;
+  public float ScaleY;
+  public float TranslateX;
+  public float TranslateY;
+
+  public Transform2D(float scaleX, float scaleY, float translateX, float translateY) {
+    ScaleX = scaleX;
+    ScaleY = scaleY;
+    TranslateX = translateX;
+    TranslateY = translateY;
+  }
+}
