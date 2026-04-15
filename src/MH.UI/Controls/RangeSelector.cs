32ea9539d1a49dd768752380d64f59019b090418
martin-holy <martin.holy@hotmail.com>
2026-04-15 09:42:32 +0200

    [N] RangeSelector

---
+++ src/MH.UI/Controls/RangeSelector.cs
@@ -0,0 +1,35 @@
+﻿namespace MH.UI.Controls;
+
+public class RangeSelector : ValueSelectorBase {
+  double _startValue;
+  double _endValue = 100;
+
+  public double StartValue {
+    get => _startValue;
+    set {
+      var snapped = Snap(value);
+
+      if (snapped > EndValue)
+        snapped = EndValue;
+
+      _setIfVary(ref _startValue, snapped);
+    }
+  }
+
+  public double EndValue {
+    get => _endValue;
+    set {
+      var snapped = Snap(value);
+
+      if (snapped < StartValue)
+        snapped = StartValue;
+
+      _setIfVary(ref _endValue, snapped);
+    }
+  }
+
+  protected override void OnRangeChanged() {
+    StartValue = _startValue;
+    EndValue = _endValue;
+  }
+}
