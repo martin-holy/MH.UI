0ae77516929b5036b70e27973fd6df9b379597f3
martin-holy <martin.holy@hotmail.com>
2026-04-15 09:42:06 +0200

    [N] ValueSelector

---
+++ src/MH.UI/Controls/ValueSelector.cs
@@ -0,0 +1,14 @@
+﻿namespace MH.UI.Controls;
+
+public class ValueSelector : ValueSelectorBase {
+  double _value;
+
+  public double Value {
+    get => _value;
+    set => _setIfVary(ref _value, Snap(value));
+  }
+
+  protected override void OnRangeChanged() {
+    Value = _value;
+  }
+}
