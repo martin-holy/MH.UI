22ad87fb4b485c29ece7cc71624cdfcfa2880da9
martin-holy <martin.holy@hotmail.com>
2026-05-15 08:53:25 +0200

    [N] SlidePanel: LayoutMode IsOverlay

--- src/MH.UI/Controls/SlidePanel.cs
+++ src/MH.UI/Controls/SlidePanel.cs
@@ -8,8 +8,11 @@ namespace MH.UI.Controls;
 public sealed class SlidePanelPinButton;
 
 public class SlidePanel : ObservableObject {
+  public enum LayoutMode { None, Overlay, Docked }
+
   private bool _canOpen = true;
   private bool _isOpen;
+  private bool _isOverlay;
   private bool _isPinned;
   private double _size;
   private double _gridSize;
@@ -20,6 +23,7 @@ public class SlidePanel : ObservableObject {
   public Dock Dock { get; }
   public bool CanOpen { get => _canOpen; set => _setCanOpen(value); }
   public bool IsOpen { get => _isOpen; set => _setIsOpen(value); }
+  public bool IsOverlay { get => _isOverlay; set => _setIsOverlay(value); }
   public bool IsPinned { get => _isPinned; set => _setIsPinned(value); }
   public double Size { get => _size; set => _setSize(value); }
   public double GridSize { get => _gridSize; set => _setGridSize(value); }
@@ -32,19 +36,29 @@ public class SlidePanel : ObservableObject {
 
   private void _setCanOpen(bool value) {
     if (!_setIfVary(ref _canOpen, value, nameof(CanOpen))) return;
-    IsOpen = _canOpen && _isPinned;
+    IsOpen = _canOpen && (_isPinned || _isOverlay);
   }
 
   private void _setIsOpen(bool value) {
     if (!_setIfVary(ref _isOpen, value, nameof(IsOpen))) return;
-    if (!_isOpen && _isPinned) IsPinned = false;
+    if (!_isOpen) {
+      IsOverlay = false;
+      IsPinned = false;
+    }
     _autoClose();
+  }
+
+  private void _setIsOverlay(bool value) {
+    if (!_setIfVary(ref _isOverlay, value, nameof(IsOverlay))) return;
+    IsOpen = _isOverlay;
+    if (_isOverlay) IsPinned = false;
   }
 
   private void _setIsPinned(bool value) {
     if (!_setIfVary(ref _isPinned, value, nameof(IsPinned))) return;
     _setGridSize();
     IsOpen = _isPinned;
+    if (_isPinned) IsOverlay = false;
   }
 
   private void _setSize(double value) {
@@ -62,25 +76,34 @@ public class SlidePanel : ObservableObject {
     GridSize = _isPinned ? _size : 0;
 
   public void OnGridMouseMove(Func<double, bool> mouseOut, bool mouseOnEdge) {
-    if (_isPinned) return;
+    if (_isPinned || _isOverlay) return;
     if (mouseOut(_size)) IsOpen = false;
     else if (mouseOnEdge && _canOpen) IsOpen = true;
   }
 
+  public void ToggleOverlay() =>
+    IsOverlay = !IsOverlay;
+
   public void TogglePinned() =>
     IsPinned = !IsPinned;
+
+  public LayoutMode GetLayoutMode() {
+    if (IsOverlay) return LayoutMode.Overlay;
+    if (IsPinned) return LayoutMode.Docked;
+    return LayoutMode.None;
+  }
 
   private async void _autoClose() {
     _cancelAutoCloseTimer();
 
-    if (!_isOpen || AutoCloseDelay == 0 || IsPinned) return;
+    if (!_isOpen || AutoCloseDelay == 0 || IsOverlay || IsPinned) return;
 
     var cts = new CancellationTokenSource();
     _autoCloseCts = cts;
 
     try {
       await Task.Delay(AutoCloseDelay, cts.Token);
-      if (!cts.IsCancellationRequested && !IsPinned) IsOpen = false;
+      if (!cts.IsCancellationRequested && !IsOverlay && !IsPinned) IsOpen = false;
     }
     catch (TaskCanceledException) { }
   }
